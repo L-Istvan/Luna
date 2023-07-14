b7ebed40d92cd35351fab8f79d9e25256e4d7e82
L.Istvan <laczipistii@gmail.com>
2023-07-14 20:20:41 +0200

    Complete web design (0.1)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+@section('content')
+<div class="d-flex justify-content-center mt-0 mt-lg-4">
+    <div class="card custom-card">
+
+        <div class="row">
+            <div class="col text-center mt-6">
+                <div class="text">Learning using a new approach</div>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 120px">
+            <div class="col-12 col-md-6 col-lg-4 mt-4">
+                <div class="container custom-container d-flex justify-content-center align-items-center float-right">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" style="color: white">
+                        <label>Szó kincs fejlesztés</label>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item" href="#">Tanult szavak gyakorlása</a></li>
+                        <li><a class="dropdown-item" href="#">Tanult szavakból olvasmány</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Ismeretlen szavak gyakorlása</a></li>
+                    </ul>
+                  </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 mt-4">
+                <div class="container custom-container d-flex justify-content-center align-items-center">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" style="color: white">
+                        <label>Szótár</label>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item" href="#">Új szótár létrehozása</a></li>
+                    </ul>
+                  </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 mt-4 ">
+                <div class="container custom-container d-flex justify-content-center align-items-center float-left">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" style="color: white">
+                        <label>Kérdés gyakorlás</label>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item" href="#">Kérdésekre válaszolás</a></li>
+                        <li><a class="dropdown-item" href="#">Kérdés feltevés</a></li>
+                    </ul>
+                  </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-4 text-center">
+                <div class="container custom-container d-flex justify-content-center align-items-center float-right">
+                    <label>Mondat ellenőrzés és javítás</label>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-4 text-center">
+                <div class="container custom-container d-flex justify-content-center align-items-center">
+                    <label>Kifejezések tanulása</label>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-4 mb-6 text-center">
+                <div class="container custom-container d-flex justify-content-center align-items-center float-left">
+                    <label>infó</label>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+        <div class="vmi mb-3">
+            <div class="input-container d-flex justify-content-center">
+                <input type="text" class="form-control custom-input"
+                placeholder="Üzenet küldés" aria-label="Large">
+            </div>
+        </div>
+</div>
+
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,32 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Luna') }}</title>
+    <link rel="icon" type="image/ico" href="{{ asset('images/favicon.ico') }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href= "{{ asset('css/header.css') }}">
+    <link rel="stylesheet" href= "{{ asset('css/index.css') }}">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    {{-- <script src="{{asset('jquery-3.7.0.min.map')}}"> --}}
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+</head>
+<body class="font-sans antialiased" style="background-color: #1A1A1B;">
+    @include('layouts.header')
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @yield('content')
+    </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+</body>
 </html>
