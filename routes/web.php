455f55bbb73ea35f57f57a75c102da1b2ecc0999
L.Istvan <laczipistii@gmail.com>
2023-08-02 00:46:55 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ReadingController;
+use App\Http\Controllers\DictionaryController;
+use PSpell\Dictionary;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,28 +22,41 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
 
 Route::get('login',function(){
     return view('auth/login')->name('login');
 });
 
+Route::get('test', function () {
+    return view('test');
+});
 
 Route::get('register',function(){
     return view(('auth/register'))->name('register');
 });
 
 Route::middleware('auth')->group(function () {
+
     Route::get('index',function () {
         return view('index');
     });
+    Route::post('chatInput',[ChatController::class,'chat']);
     Route::get('chat/tanult_szavak', [ChatController::class,'practicingLearnedWords'])->name('chat.practicingLearnedWords');
-    Route::get('chat/olvasas',[ChatController::class,'readingFromLearnedWords'])->name('chat.readingFromLearnedWords');
+
+    Route::get('/olvasas/valasztas',[ReadingController::class,'index'])->name('reading.index');
+    Route::get('/olvasas/ismeretlen_szavakkal',[ReadingController::class,'individually'])->name('reading.individually');
+
+    Route::get('szotar/letrehozas',[DictionaryController::class,'create'])->name('dictionary.create');
+    Route::get('szotar/{szerkesztes}',[DictionaryController::class,'edit'])->name('dictionary.edit');
+    Route::post('szotar/createTable',[DictionaryController::class,'store'])->name('dictionary.store');
+    Route::post('szotar/addCells',[DictionaryController::class,'storeCells'])->name('dictionary.storeCells');
+    Route::patch('szotar/updateCells',[DictionaryController::class,'update'])->name('dictionary.update');
+    Route::delete('szotar/deleteCells',[DictionaryController::class,'destroy'])->name('dictionary.destroy');
+
+
     Route::get('chat/idegen_szavak',[ChatController::class,'practicingUnknownWords'])->name('chat.practicingUnknownWords');
-    Route::get('chat/szotar',[ChatController::class,'createNewDictionary'])->name('chat.createNewDictionary');
+
+    Route::post('chat/tanult_szavakbol_olvasmany',[ChatController::class,'generateTextFromSavedWords'])->name('reading.generateTextFromSavedWords');
     Route::get('chat/kerdes_felteves',[ChatController::class,'answersToQuestions'])->name('chat.answersToQuestions');
     Route::get('chat/kersesre_valasz',[ChatController::class,'askingQuestion'])->name('chat.askingQuestion');
     Route::get('chat/mondat_ellenorzes',[ChatController::class,'sentenceCheck'])->name('chat.sentenceCheck');
