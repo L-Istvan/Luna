e58ffe8188919852d8c39a8c5491e234742c81a8
L.Istvan <laczipistii@gmail.com>
2023-08-16 21:07:08 +0200

    Debug off

--- config/logging.php
+++ config/logging.php
@@ -61,7 +61,7 @@ return [
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'emergency',
             'replace_placeholders' => true,
         ],
 
